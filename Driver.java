d4db9e7d35c66d2f30f81903bd4d54df0326d6bd
Mcplsmz <yitongzhou8@gmail.com>
2025-06-01 23:54:47 -0400

    “added

--- Driver.java
+++ Driver.java
@@ -1,12 +1,30 @@
 public class Driver { 
 	public static void main(String [] args) { 
 		Polynomial p = new Polynomial(); 
-		System.out.println(p.evaluate(3)); 
-		double [] c1 = {6,0,0,5}; 
-		Polynomial p1 = new Polynomial(c1); 
-		double [] c2 = {0,-2,0,0,-9}; 
-		Polynomial p2 = new Polynomial(c2); 
-		Polynomial s = p1.add(p2); 
+		//System.out.println(p.evaluate(3)); 
+		double [] c2 = {6,1,1,5};
+		int [] d2 = {0,1,2,3};
+		 
+		double [] c1 = {1,1,1,1,1};
+		int [] d1 = {1,2,3,4,5};
+		Polynomial p1 = new Polynomial(c1,d1);
+		Polynomial p2 = new Polynomial(c2,d2); 
+		Polynomial s = p1.multiply(p2);
+		
+		for(int i = 0; i<s.pow.length; i++) {
+			
+			
+			System.out.println(s.coef[i]);
+			System.out.println(s.pow[i]);
+			System.out.println(" _________________ ");
+			
+			
+		}
+		
+		System.out.println(p1.evaluate(1));
+		System.out.println(p2.evaluate(1));
+		System.out.println(s.evaluate(1));
+		
 		System.out.println("s(0.1) = " + s.evaluate(0.1)); 
 		if(s.hasRoot(1)) 
 			System.out.println("1 is a root of s");
